98cdc404bf79ea41dcf4fc7b19285a9dd8015fe4
unknown <admin@radsl.net>
2025-03-16 12:24:23 -0700

    fix typo

--- lib/models.py
+++ lib/models.py
@@ -64,7 +64,7 @@ default_xtts_settings = {
 }
 default_bark_settings = {
     "samplerate": 24000,
-    "files": ["coarse_2.pt'],
+    "files": ['coarse_2.pt'],
     "voices": {"KumarDahl": os.path.join(voices_dir, "eng", "adult", "male", "bark","KumarDahl", "KumarDahl.npz"}
 }
 default_vits_settings = {

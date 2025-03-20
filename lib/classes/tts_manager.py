7c8e302d735f075526cbb023df982c9ae3cde8e2
unknown <admin@radsl.net>
2025-03-19 20:15:24 -0700

    test bark#23

--- lib/classes/tts_manager.py
+++ lib/classes/tts_manager.py
@@ -470,9 +470,9 @@ class TTSManager:
                         bark_dir = f"{os.path.dirname(default_bark_settings['voices']['KumarDahl'])}/"
                         voice_key = re.sub(r'.npz$', '', os.path.basename(default_bark_settings['voices']['KumarDahl']))
                     speaker_argument = {
-                        #"voice_dir": bark_dir,
-                        #"speaker_wav": voice_key, 
-                        #"speaker_wav": f"{os.path.splitext(os.path.basename(default_bark_settings['voices']['KumarDahl']))[0]}.wav",
+                        "voice_dir": bark_dir,
+                        "speaker": voice_key, 
+                        "speaker_wav": os.path.join(os.path.dirname(bark_dir), f"{os.path.splitext(os.path.basename(default_bark_settings['voices']['KumarDahl']))[0]}.wav"),
                         "speaker_embedding": default_bark_settings['voices']['KumarDahl']
                     }                      
                     with torch.no_grad():

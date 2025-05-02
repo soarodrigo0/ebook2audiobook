7ea21c9aab3769946a8c0f90e8b01fbb5374de50
unknown <admin@radsl.net>
2025-05-02 15:32:29 -0700

    warning text updated

--- lib/functions.py
+++ lib/functions.py
@@ -596,9 +596,9 @@ def get_chapters(epubBook, session):
             ***************************************************************************************
                                             NOTE: THE WARNING
                                 "Character xx not found in the vocabulary."
-            MEANS THE MODEL CANNOT INTERPRET THE CHARACTER AND WILL MAYBE GENERATE AN HALLUCINATION
-            TO IMPROVE THIS MODEL IT NEEDS TO ADD THIS CHARACTER INTO A NEW TRAINING MODEL.
-            YOU CAN IMPROVE IT OR ASK TO A MODEL TRAINING DEVELOPER.
+            MEANS THE MODEL CANNOT INTERPRET THE CHARACTER AND WILL MAYBE GENERATE (AS WELL AS WRONG PUNCTUATION POSITION)
+            AN HALLUCINATION TO IMPROVE THIS MODEL IT NEEDS TO ADD THIS CHARACTER INTO A NEW TRAINING MODEL. YOU CAN
+            IMPROVE IT OR ASK TO A MODEL TRAINING DEVELOPER.
             ***************************************************************************************
         '''
         print(msg)

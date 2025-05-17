07108242716b160a89b433d7d4bc828b03adc9e2
unknown <admin@radsl.net>
2025-05-17 13:52:53 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -915,7 +915,7 @@ def convert_chapters_to_audio(session):
             progress_bar = gr.Progress(track_tqdm=True)        
         tts_manager = TTSManager(session, is_gui_process)
         if not tts_manager.params['tts']:
-            error = f"TTS engine {session['tts_engine']} could not be loaded!\nPossible reason can be not enough VRAM/RAM memory.\nTry to lower max_tts_in_memory in ./lib/conf.py"
+            error = f"TTS engine {session['tts_engine']} could not be loaded!\nPossible reason can be not enough VRAM/RAM memory.\nTry to lower max_tts_in_memory in ./lib/models.py"
             print(error)
             return False
         resume_chapter = 0

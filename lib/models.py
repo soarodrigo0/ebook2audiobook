afef4cd217b422952ee3e13ca28c981edb907c5a
unknown <admin@radsl.net>
2025-05-27 17:11:45 -0700

    ...

--- lib/models.py
+++ lib/models.py
@@ -66,8 +66,8 @@ default_xtts_settings = {
 }
 default_bark_settings = {
     "samplerate": 24000,
-    "text_temp": 0.33,
-    "waveform_temp": 0.33,
+    "text_temp": 0.26,
+    "waveform_temp": 0.15,
     "files": ['coarse_2.pt'],
     "voices": {}
 }

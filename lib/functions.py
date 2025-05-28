5a0cb823929fc2fcf459ac9f2c56ac1da333c614
unknown <admin@radsl.net>
2025-05-28 13:03:00 -0700

    ....

--- lib/functions.py
+++ lib/functions.py
@@ -1831,17 +1831,17 @@ def web_interface(args):
                     display: none !important;
                 }
                 ///////////////
-                #voice_player {
+                #gr_voice_player {
                     display: block !important;
                     margin: 0 !important;
                     padding: 0 !important;
                     width: 60px !important;
                     height: 60px !important;
                 }
-                #voice_player :is(#waveform, .rewind, .skip, .playback, label, .volume, .empty) {
+                #gr_voice_player :is(#waveform, .rewind, .skip, .playback, label, .volume, .empty) {
                     display: none !important;
                 }
-                #voice_player .controls {
+                #gr_voice_player .controls {
                     display: block !important;
                     position: absolute !important;
                     left: 15px !important;
@@ -1886,11 +1886,11 @@ def web_interface(args):
                             gr_ebook_mode = gr.Radio(label='', elem_id='gr_ebook_mode', choices=[('File','single'), ('Directory','directory')], value='single', interactive=True)
                         with gr.Group():
                             gr_language = gr.Dropdown(label='Language', elem_id='gr_language', choices=language_options, value=default_language_code, type='value', interactive=True)
-                        gr_group_voice_file = gr.Group(visible=visible_gr_group_voice_file)
+                        gr_group_voice_file = gr.Group(elem_id='gr_group_voice_file', visible=visible_gr_group_voice_file)
                         with gr_group_voice_file:
                             gr_voice_file = gr.File(label='*Cloning Voice Audio Fiie', elem_id='gr_voice_file', file_types=voice_formats, value=None, height=140)
                             with gr.Row():
-                                gr_voice_player = gr.Audio(elem_id='voice_player', type='filepath', interactive=False, show_download_button=False, container=False, visible=False, show_share_button=False, show_label=False, waveform_options=gr.WaveformOptions(show_controls=False), scale=0, min_width=60)
+                                gr_voice_player = gr.Audio(elem_id='gr_voice_player', type='filepath', interactive=False, show_download_button=False, container=False, visible=False, show_share_button=False, show_label=False, waveform_options=gr.WaveformOptions(show_controls=False), scale=0, min_width=60)
                                 gr_voice_list = gr.Dropdown(label='', elem_id='gr_voice_list', choices=voice_options, type='value', interactive=True, scale=2)
                                 gr_voice_del_btn = gr.Button('🗑', elem_id='gr_voice_del_btn', elem_classes=['small-btn'], variant='secondary', interactive=True, visible=False, scale=0, min_width=60)
                             gr.Markdown('<p>&nbsp;&nbsp;* Optional</p>')

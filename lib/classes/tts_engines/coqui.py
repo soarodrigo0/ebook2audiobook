54a71af670614cd5a0a982a2990db813c3104a75
unknown <admin@radsl.net>
2025-05-31 10:51:49 -0700

    ...

--- lib/classes/tts_engines/coqui.py
+++ lib/classes/tts_engines/coqui.py
@@ -213,9 +213,9 @@ class Coqui:
                     tts.load_checkpoint(
                         config,
                         checkpoint_dir=checkpoint_dir,
-                        checkpoint_path=checkpoint_path,
-                        vocab_path=vocab_path,
-                        speakers_path=speakers_path,
+                        #checkpoint_path=checkpoint_path,
+                        #vocab_path=vocab_path,
+                        #speakers_path=speakers_path,
                         use_deepspeed=default_xtts_settings['use_deepspeed'],
                         eval=True
                     )
@@ -228,9 +228,9 @@ class Coqui:
                     tts.load_checkpoint(
                         config,
                         checkpoint_dir=checkpoint_dir,
-                        text_model_path=text_model_path,
-                        coarse_model_path=coarse_model_path,
-                        fine_model_path=fine_model_path,
+                        #text_model_path=text_model_path,
+                        #coarse_model_path=coarse_model_path,
+                        #fine_model_path=fine_model_path,
                         eval=True
                     )
                 else:

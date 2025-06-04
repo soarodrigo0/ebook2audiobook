fcc7740f5cf055be5b430a6635620712ac366a1b
unknown <admin@radsl.net>
2025-06-03 19:07:34 -0700

    ...

--- lib/classes/tts_engines/coqui.py
+++ lib/classes/tts_engines/coqui.py
@@ -266,7 +266,6 @@ class Coqui:
 
     def _check_xtts_builtin_speakers(self, voice_path, speaker, device):
         try:
-            print(f'---------------- {voice_path} --------------')
             voice_parts = Path(voice_path).parts
             if self.session['language'] not in voice_parts:               
                 if speaker in default_xtts_settings['voices'].keys() and self.session['language'] in language_tts[XTTSv2].keys():

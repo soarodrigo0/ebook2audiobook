15c836d3f413b5e8e3203a071d68cdb5ff1b345f
unknown <admin@radsl.net>
2025-06-11 03:34:32 -0700

    ...

--- lib/models.py
+++ lib/models.py
@@ -98,13 +98,13 @@ default_bark_settings = {
 	"rating": {"GPU VRAM": 4, "CPU": 1, "RAM": 16, "Emotions": 4}
 }
 default_tacotron_settings = {
-    "samplerate": 16000,
+    "samplerate": 22050,
     "files": ['config.json', 'best_model.pth', 'vocoder_config.json', 'vocoder_model.pth'],
     "voices": {},
     "rating": {"GPU VRAM": 2, "CPU": 3, "RAM": 4, "Emotions": 2}
 }
 default_vits_settings = {
-    "samplerate": 22050,
+    "samplerate": 16000,
     "files": ['config.json', 'model_file.pth', 'language_ids.json'],
     "voices": {},
     "rating": {"GPU VRAM": 2, "CPU": 3, "RAM": 4, "Emotions": 2}

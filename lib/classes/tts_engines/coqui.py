fe0a81aa603b49e3cc56660cd4c8dad62f8b7feb
unknown <admin@radsl.net>
2025-06-20 08:47:36 -0700

    ...

--- lib/classes/tts_engines/coqui.py
+++ lib/classes/tts_engines/coqui.py
@@ -547,9 +547,9 @@ class Coqui:
                 self.session['voice'] if self.session['voice'] is not None 
                 else os.path.join(self.session['custom_model_dir'], self.session['tts_engine'], self.session['custom_model'], 'ref.wav') if self.session['custom_model'] is not None
                 else models[self.session['tts_engine']][self.session['fine_tuned']]['voice']
-            )          
+            )    
+            speaker = re.sub(r'_(24000|16000)\.wav$|\.npz$', '', os.path.basename(settings['voice_path']))
             if settings['voice_path'] is not None and settings['voice_path'] not in default_engine_settings[TTS_ENGINES['BARK']]['voices'].keys() and os.path.basename(settings['voice_path']) != 'ref.wav':
-                speaker = re.sub(r'(_16000|_24000).wav$', '', os.path.basename(settings['voice_path']))
                 self.session['voice'] = settings['voice_path'] = self._check_xtts_builtin_speakers(settings['voice_path'], speaker, self.session['device'])
                 if not settings['voice_path']:
                     msg = f"Could not create the builtin speaker selected voice in {self.session['language']}"

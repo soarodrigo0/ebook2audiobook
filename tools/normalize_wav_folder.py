0c4ad202df9f4b856854514560186217d7ee5303
unknown <admin@radsl.net>
2025-06-21 04:01:21 -0700

    ...

--- tools/normalize_wav_folder.py
+++ tools/normalize_wav_folder.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import argparse
 import torch
+import shutil
 
 def demucs_voice(wav_file, output_dir, models_dir):
 	try:
@@ -47,36 +48,60 @@ def normalize_audio_folder(folder_path):
         for file in files:
             if file.lower().endswith('.wav'):
                 input_file = os.path.join(root, file)
-                temp_file = os.path.join(root, 'temp_output.wav')  # Temporary file to avoid overwriting during processing
+                temp_file = 
                 models_dir = os.path.join('..', 'models', 'tts')
                 demucs_file = demucs_voice(input_file, folder_path, models_dir)
-                ffmpeg_cmd = [
-                    'ffmpeg', '-i', demucs_file,
-                    '-af', 'agate=threshold=-25dB:ratio=1.4:attack=10:release=250,'
-                           'afftdn=nf=-70,'
-                           'acompressor=threshold=-20dB:ratio=2:attack=80:release=200:makeup=1dB,'
-                           'loudnorm=I=-16:TP=-3:LRA=7:linear=true,'
-                           'equalizer=f=150:t=q:w=2:g=1,'
-                           'equalizer=f=250:t=q:w=2:g=-3,'
-                           'equalizer=f=3000:t=q:w=2:g=2,'
-                           'equalizer=f=5500:t=q:w=2:g=-4,'
-                           'equalizer=f=9000:t=q:w=2:g=-2,'
-                           'highpass=f=63',
-                    '-y', temp_file
+                process_file = os.path.join(root, 'temp_output.wav')  # Temporary file to avoid overwriting during processing
+                ffmpeg_cmd = [shutil.which('ffmpeg'), '-hide_banner', '-nostats', '-i', demucs_file]
+                filter_complex = (
+                    'agate=threshold=-25dB:ratio=1.4:attack=10:release=250,'
+                    'afftdn=nf=-70,'
+                    'acompressor=threshold=-20dB:ratio=2:attack=80:release=200:makeup=1dB,'
+                    'loudnorm=I=-14:TP=-3:LRA=7:linear=true,'
+                    'equalizer=f=150:t=q:w=2:g=1,'
+                    'equalizer=f=250:t=q:w=2:g=-3,'
+                    'equalizer=f=3000:t=q:w=2:g=2,'
+                    'equalizer=f=5500:t=q:w=2:g=-4,'
+                    'equalizer=f=9000:t=q:w=2:g=-2,'
+                    'highpass=f=63[audio]'
+                )
+                ffmpeg_cmd += [
+                    '-filter_complex', filter_complex,
+                    '-map', '[audio]',
+                    '-ar', 'null',
+                    '-y', process_file
                 ]
-
                 try:
-                    print(f"Processing file: {input_file}")
-                    subprocess.run(ffmpeg_cmd, check=True)
-                    os.replace(temp_file, input_file)
-                    print(f"File processed and replaced: {input_file}")
+                    process = subprocess.Popen(
+                        ffmpeg_cmd,
+                        env={},
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT,
+                        text=True,
+                        universal_newlines=True,
+                        encoding='utf-8'
+                    )
+                    for line in process.stdout:
+                        print(line, end='')  # Print each line of stdout
+                    process.wait()
+                    if process.returncode != 0:
+                        error = f'normalize_audio(): process.returncode: {process.returncode}'
+                        break
+                    elif not os.path.exists(output_file) or os.path.getsize(output_file) == 0:
+                        error = f'normalize_audio() error: {output_file} was not created or is empty.'
+                        break
+                    else:
+                        os.replace(process_file, input_file)
+                        print(f"File processed and replaced: {input_file}")
+                except subprocess.CalledProcessError as e:
+                    error = f'_normalize_audio() ffmpeg.Error: {e.stderr.decode()}'
+                    break
                 except subprocess.CalledProcessError as e:
                     print(f"Error processing file {input_file}: {e}")
+                    break
                 except Exception as e:
                     print(f"Unexpected error: {e}")
-                finally:
-                    if os.path.exists(temp_file):
-                        os.remove(temp_file)
+                    break
 
 folder_path = '../assets/bark'
 normalize_audio_folder(folder_path)

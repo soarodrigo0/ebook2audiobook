9d6c5b1294836710a83b98be456ce58ef12a2f82
unknown <admin@radsl.net>
2025-06-29 18:20:08 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -501,6 +501,13 @@ def normalize_text(text, lang, lang_iso1, tts_engine):
         # Ensure spaces before & after `,` and `.` ONLY when NOT between numbers
         comma_dot_pattern = r'(?<!\d)\s*(\.{3}|[,.])\s*(?!\d)'
         text = re.sub(comma_dot_pattern, r' \1 ', text)
+    # Replace special chars with words
+    specialchars = specialchars_mapping[lang] if lang in specialchars_mapping else specialchars_mapping['eng']
+    for char, word in specialchars.items():
+        text = text.replace(char, f" {word} ")
+    for char in specialchars_remove:
+        text = text.replace(char, ' ')
+    text = ' '.join(text.split())
 
     return text
 

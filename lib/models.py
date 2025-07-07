a1302dbb4732ecd68c97732658e116c82f9c89f7
unknown <admin@radsl.net>
2025-07-07 05:45:09 -0700

    ...

--- lib/models.py
+++ lib/models.py
@@ -24,8 +24,8 @@ voice_conversion_models/multilingual/multi-dataset/openvoice_v2
 default_vc_model = "voice_conversion_models/multilingual/multi-dataset/knnvc"
 
 max_tts_in_memory = 2 # TTS engines to keep in memory (1 tts engine ~= 4GB to 8GB RAM).
-max_custom_model = 10
-max_custom_voices = 100
+max_custom_model = 100
+max_custom_voices = 1000
 max_upload_size = '6GB'
 
 default_engine_settings = {

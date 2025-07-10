8ac94d3d626bfc70a53247ba4640ff92ef61e8bd
unknown <admin@radsl.net>
2025-07-10 05:37:40 -0700

    ...

--- lib/classes/voice_extractor.py
+++ lib/classes/voice_extractor.py
@@ -84,9 +84,9 @@ class VoiceExtractor:
             status, report = detector.detect(
                 frame_s=1.0,
                 overlap=0.5,
-                rms_db_thresh=-17,    # absolute –17 dB cutoff on loudness
-                flatness_thresh=0.02, # per our calibration
-                zcr_thresh=0.11       # per our calibration
+                rms_db_thresh=-19,    # absolute –17 dB cutoff on loudness
+                flatness_thresh=0.15, # per our calibration
+                zcr_thresh=0.09       # per our calibration
             )
             print(report)
             if status:

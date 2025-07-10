4f52c33e77e56263ad23d7f4e7f5cd01b5a743da
unknown <admin@radsl.net>
2025-07-10 06:13:55 -0700

    ...

--- lib/classes/background_detector.py
+++ lib/classes/background_detector.py
@@ -19,8 +19,6 @@ class BackgroundDetector:
         self.model.eval()
         if torch.cuda.is_available():
             self.model.cuda()
-        with open(os.environ['HF_TOKEN_PATH']) as f:
-            os.environ['HUGGINGFACE_TOKEN'] = f.read().strip()
         self.vad_pipeline = Pipeline.from_pretrained("pyannote/voice-activity-detection", use_auth_token=os.environ['HUGGINGFACE_TOKEN'])
 
     def _compute_vggish_energy(self, log_mel):

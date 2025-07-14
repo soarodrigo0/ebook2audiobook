09a5e798b3df868e14d1691f777172793cae6cc4
unknown <admin@radsl.net>
2025-07-13 19:51:01 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -562,6 +562,7 @@ YOU CAN IMPROVE IT OR ASK TO A TRAINING MODEL EXPERT.
                                     result.append(sentence[last_pos:])
                                     sentence = ''.join(result)
                     if session['tts_engine'] not in [TTS_ENGINES['XTTSv2'], TTS_ENGINES['BARK']]:
+                        sentence = re.sub(r'(\d)\)', r'\1 :', sentence)
                         sentence = math2word(sentence, session['language'], session['language_iso1'], session['tts_engine'], is_num2words_compat)
                     sentences_list[i] = sentence
                 chapters.append(sentences_list)
@@ -1028,7 +1029,6 @@ def math2word(text, lang, lang_iso1, tts_engine, is_num2words_compat):
         return match.group(0)
 
     # 0) Turn ". 1)" → ". 1."
-    text = re.sub(r'\n(\d)\)', r'\n\1 :', text)
     # Pre-process formatted series (e.g. phone numbers) if needed
     text = check_formatted_number(text, lang_iso1, is_num2words_compat)
     # Symbol phonemes

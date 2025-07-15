fd9f0c64819992551eeaa815239a3d5d6b5af03d
unknown <admin@radsl.net>
2025-07-15 07:06:24 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -643,9 +643,9 @@ def filter_chapter(doc, lang, lang_iso1, tts_engine, stanza_nlp, is_num2words_co
         specialchars_remove_table = str.maketrans({ch: ' ' for ch in specialchars_remove})
         text = text.translate(specialchars_remove_table)
         # Ensure space before and after punctuation (excluding `,` and `.`)
-        pattern_space = re.escape(''.join(punctuation_list))
-        punctuation_pattern_space = r'\s*([{}])\s*'.format(pattern_space)
-        text = re.sub(punctuation_pattern_space, r' \1 ', text)
+        #pattern_space = re.escape(''.join(punctuation_list))
+        #punctuation_pattern_space = r'\s*([{}])\s*'.format(pattern_space)
+        #text = re.sub(punctuation_pattern_space, r' \1 ', text)
         return get_sentences(text, lang, tts_engine)
     except Exception as e:
         DependencyError(e)

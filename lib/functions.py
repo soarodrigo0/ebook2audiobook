cbe2b6588c0a600cd6beecf430d2b0e734ded8ee
unknown <admin@radsl.net>
2025-07-16 18:19:05 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2061,7 +2061,7 @@ def web_interface(args, ctx):
                     position: fixed;
                     top: 0; left: 0;
                     width: 100vw; height: 100vh;
-                    background: transparent;
+                    background: rgba(255,255,255,0.3);
                     display: flex;
                     align-items: center;
                     justify-content: center;

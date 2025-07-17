baed347b60c0a2ba974dd8ef19b6f19cd48c1db7
unknown <admin@radsl.net>
2025-07-17 07:08:50 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2475,7 +2475,6 @@ def web_interface(args, ctx):
                 ### BARK Params
                 session['text_temp'] = session['text_temp'] if session['text_temp'] else default_engine_settings[TTS_ENGINES['BARK']]['text_temp']
                 session['waveform_temp'] = session['waveform_temp'] if session['waveform_temp'] else default_engine_settings[TTS_ENGINES['BARK']]['waveform_temp']
-                _ = 1 / 0
                 return (
                     gr.update(value=ebook_data), gr.update(value=session['ebook_mode']), gr.update(value=session['device']),
                     gr.update(value=session['language']), update_gr_voice_list(id), update_gr_tts_engine_list(id), update_gr_custom_model_list(id),

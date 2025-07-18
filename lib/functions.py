bde9155b53dbf805fd3183ca31b8b0ab1e1fcb78
unknown <admin@radsl.net>
2025-07-17 19:32:09 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2731,13 +2731,15 @@ def web_interface(args, ctx):
                 if default_voice == None:
                     session['voice'] = default_voice
                 else:
-                    if session['voice'] != default_voice:                    
+                    if session['voice'] != None:                  
                         default_voice_index = next((i for i, opt in enumerate(voice_options) if opt[1] == default_voice), 0)
                         session['voice'] = (
                             session.get('voice')
                             if session.get('voice') in [opt[1] for opt in voice_options]
                             else voice_options[default_voice_index][1]
                         )
+                    else:
+                        session['voice'] = default_voice
                 return gr.update(choices=voice_options, value=session['voice'])
             except Exception as e:
                 error = f'update_gr_voice_list(): {e}!'

e6d04562b9216018e2886ef8a0937fb7a5ed3e85
unknown <admin@radsl.net>
2025-07-17 20:02:26 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2735,7 +2735,7 @@ def web_interface(args, ctx):
                         session['voice'] = (
                             session.get('voice')
                             if session.get('voice') in [opt[1] for opt in voice_options]
-                            else default_voice
+                            else voice_options[0][1]
                         )
                     else:
                         session['voice'] = default_voice

2d3497037f9100f164b022f74c144f2ef9b4a2c3
unknown <admin@radsl.net>
2025-07-29 08:02:46 -0700

    ...

--- lib/classes/tts_engines/coqui.py
+++ lib/classes/tts_engines/coqui.py
@@ -741,7 +741,7 @@ class Coqui:
                         self.audio_segments.append(audio_tensor)
                         if re.search(r'\w', sentence, flags=re.UNICODE):
                             silence_tensor = torch.zeros(1, int(settings['samplerate'] * (int(np.random.uniform(0.8, 1.6) * 100) / 100))) # 0.8 to 1.6 seconds
-                            audio_segments.append(silence_tensor.clone())
+                            self.audio_segments.append(silence_tensor.clone())
                     if self.audio_segments and torch.equal(self.audio_segments[-1], silence_tensor):
                         self.audio_segments = self.audio_segments[:-1]
                     if self.audio_segments:

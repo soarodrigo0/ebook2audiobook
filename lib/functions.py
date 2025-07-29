916d863b8de7b0031d683cc6096dd1436853f349
unknown <admin@radsl.net>
2025-07-29 07:21:17 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -773,6 +773,7 @@ def get_sentences(text, lang, tts_engine):
     # Step 1: Split first by ‡pause‡, keeping it as a separate element
     sentences = re.split(rf'({re.escape(TTS_SML["pause"])})', text)
     sentences = [s if s == TTS_SML['pause'] else s.strip() for s in sentences if s.strip() or s == TTS_SML['pause']]
+    print(sentences)
     if lang in ['zho', 'jpn', 'kor', 'tha', 'lao', 'mya', 'khm']:
         result = []
         for s in sentences:

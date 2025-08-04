efa2da33db5e53b08ba8bd28b7c32aa1b254479b
unknown <admin@radsl.net>
2025-08-04 06:58:39 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -697,9 +697,9 @@ def filter_chapter(doc, lang, lang_iso1, tts_engine, stanza_nlp, is_num2words_co
         text = text.translate(specialchars_remove_table)
         text = normalize_text(text, lang, lang_iso1, tts_engine)
         # Ensure space before and after punctuation_list
-        pattern_space = re.escape(''.join(punctuation_list))
-        punctuation_pattern_space = r'\s*([{}])\s*'.format(pattern_space)
-        text = re.sub(punctuation_pattern_space, r' \1 ', text)
+        #pattern_space = re.escape(''.join(punctuation_list))
+        #punctuation_pattern_space = r'\s*([{}])\s*'.format(pattern_space)
+        #text = re.sub(punctuation_pattern_space, r' \1 ', text)
         return get_sentences(text, lang, tts_engine)
     except Exception as e:
         error = f'filter_chapter() error: {e}'

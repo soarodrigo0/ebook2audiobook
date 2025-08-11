d67397d44aa3dc4acfaae0c711c46199cae2e758
unknown <admin@radsl.net>
2025-08-11 06:40:55 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -3684,14 +3684,11 @@ def web_interface(args, ctx):
                                                                         textarea.style.height =  '100%';
                                                                         textarea.style.textAlign = 'center';
                                                                         textarea.style.marginTop = 'auto';
-                                                                        textarea.style.opacity = '0';
-                                                                        textarea.value = this.activeCues[0].text;
                                                                         textarea.style.transition = 'opacity 0.4s ease-in';
-                                                                        textarea.dataset.faded = 'true';
-                                                                        // Force browser reflow before changing opacity
-                                                                        requestAnimationFrame(() => {
-                                                                            textarea.style.opacity = '0';
-                                                                        });
+                                                                        textarea.style.opacity = '0'; // start hidden
+                                                                        void textarea.offsetWidth;
+                                                                        textarea.value = this.activeCues[0].text;
+                                                                        textarea.style.opacity = '1';
                                                                     }
                                                                 }
                                                             }

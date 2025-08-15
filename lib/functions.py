b289acb6e02866dd5604e5a9acd48a2ba194b5dc
unknown <admin@radsl.net>
2025-08-15 04:46:28 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -69,10 +69,10 @@ class SessionTracker:
     def __init__(self):
         self.lock = threading.Lock()
 
-    def start_session(self, status, id):
+    def start_session(self, id):
         with self.lock:
-            if status is None:
-                session = context.get_session(id)
+            session = context.get_session(id)
+            if session['status'] is None:
                 session['status'] = 'ready'
                 return True
         return False
@@ -3475,13 +3475,13 @@ def web_interface(args, ctx):
                         session = context.get_session(data['id'])
                         session_id = session['id']
                         if data.get('tab_id') == session.get('tab_id') or data.get('tab_id') is None:
-                            data['status'] = None 
-                            data['cancellation_requested'] = False
-                        if not ctx_tracker.start_session(data['status'], session_id):
+                            session['status'] = None 
+                        restore_session_from_data(data, session)
+                        if not ctx_tracker.start_session(session_id):
                             session_id = ''
                             error = "Your session is already active.<br>If it's not the case please close your browser and relaunch it."
                             return gr.update(), gr.update(), gr.update(value=''), update_gr_glass_mask(str=error)
-                        restore_session_from_data(data, session)
+                        session['cancellation_requested'] = False
                         if isinstance(session['ebook'], str):
                             if not os.path.exists(session['ebook']):
                                 session['ebook'] = None

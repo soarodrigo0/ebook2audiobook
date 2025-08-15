b5ab2118139e0ecc1e7b158f335ee9b21e4fc301
unknown <admin@radsl.net>
2025-08-15 04:57:19 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -2263,7 +2263,6 @@ def show_alert(state):
 def web_interface(args, ctx):
     global context, is_gui_process
     context = ctx
-    session_id = str(uuid.uuid4())
     script_mode = args['script_mode']
     is_gui_process = args['is_gui_process']
     is_gui_shared = args['share']
@@ -2485,6 +2484,7 @@ def web_interface(args, ctx):
     '''
     
     with gr.Blocks(theme=theme, title=title, css=header_css, delete_cache=(86400, 86400)) as app:
+        session_id = str(uuid.uuid4())
         with gr.Tabs(elem_id='gr_tabs'):
             gr_tab_main = gr.TabItem('Main Parameters', elem_id='gr_tab_main', elem_classes='tab_item')
             with gr_tab_main:
@@ -3496,7 +3496,6 @@ def web_interface(args, ctx):
                                 if session['tts_engine'] in models.keys():
                                     if session['fine_tuned'] not in models[session['tts_engine']].keys():
                                         session['fine_tuned'] = default_fine_tuned
-                                        
                                 else:
                                     session['tts_engine'] = default_tts_engine
                                     session['fine_tuned'] = default_fine_tuned

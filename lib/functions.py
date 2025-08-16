680fb65b2df88545919f1f30c0f93cb5206b3bb1
unknown <admin@radsl.net>
2025-08-15 19:37:09 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -3477,7 +3477,7 @@ def web_interface(args, ctx):
                 if data is None:
                     data = context.get_session(str(uuid.uuid4()))
                 session = context.get_session(data['id'])
-                if data.get('tab_id') == session.get('tab_id') or data['status'] is None:
+                if data.get('tab_id') == session.get('tab_id') or session.get('tab_id') is None:
                     restore_session_from_data(data, session)
                     session[req.session_hash] = req.session_hash
                     session['cancellation_requested'] = False
@@ -4062,7 +4062,6 @@ def web_interface(args, ctx):
                                     const saved = JSON.parse(localStorage.getItem('data') || '{}');
                                     if (saved.tab_id === tab_id || !saved.tab_id) {
                                         saved.tab_id = null
-                                        saved.status = null;
                                         localStorage.setItem('data', JSON.stringify(saved));
                                     }
                                 }catch(e){

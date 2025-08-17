86a04defa1a3ff54a535c2f470e80f277e45f8db
unknown <admin@radsl.net>
2025-08-16 20:08:31 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -3923,13 +3923,11 @@ def web_interface(args, ctx):
                                                 gr_audiobook_player.addEventListener('timeupdate', () => {
                                                     const playback_time = gr_audiobook_player.currentTime || 0;
                                                     const now = performance.now();
-                                                    console.log('playback_time', playback_time)
-                                                    console.log('now', now)
-                                                    if (now - last_time > 3000) {
+                                                    //if (now - last_time > 3000) {
                                                         gr_audiobook_player_playback_time.value = String(playback_time);
                                                         gr_audiobook_player_playback_time.dispatchEvent(new Event("input", { bubbles: true }));
                                                         last_time = now;
-                                                    }
+                                                    //}
                                                     const cue = findCue(cues, playback_time);
                                                     if (cue && cue !== lastCue) {
                                                         if (fade_timeout) {

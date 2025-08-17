854b2c4de51cbce1ac39246bee190f0c7a4c4be8
unknown <admin@radsl.net>
2025-08-17 05:51:22 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -3910,17 +3910,18 @@ def web_interface(args, ctx):
                                                 let lastCue = null;
                                                 let fade_timeout = null;
                                                 let last_time = 0;
-                                                gr_audiobook_player.addEventListener('canplay', () => {
+                                                gr_audiobook_player.addEventListener('loaded', () => {
                                                     const stored = window.localStorage.getItem('data');
                                                     if(stored){
                                                         const parsed = JSON.parse(stored);
-                                                        const playback_time = parseFloat(parsed.playback_time || 0);
-                                                        console.log('playback_time:', playback_time);
-                                                        gr_audiobook_player.currentTime = playback_time;
-                                                        gr_audiobook_player_playback_time.value = playback_time;
-                                                        gr_audiobook_player_playback_time.dispatchEvent(new Event("input", { bubbles: true }));
+                                                        const playback_time = (parsed.playback_time) ? parseFloat(parsed.playback_time) : undefined;
+                                                        if (playback_time) {
+                                                            console.log('playback_time:', playback_time);
+                                                            gr_audiobook_player_playback_time.value = playback_time;
+                                                            gr_audiobook_player_playback_time.dispatchEvent(new Event("input", { bubbles: true }));
+                                                        }
                                                     }
-                                                },{once: true});
+                                                });
                                                 gr_audiobook_player.addEventListener('timeupdate', () => {
                                                     const playback_time = gr_audiobook_player.currentTime || 0;
                                                     const cue = findCue(cues, playback_time);

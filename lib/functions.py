e9ea828545caed8a4df93e2e88a58b9b9b3ba7b6
unknown <admin@radsl.net>
2025-08-16 20:04:30 -0700

    ...

--- lib/functions.py
+++ lib/functions.py
@@ -3922,6 +3922,13 @@ def web_interface(args, ctx):
                                                 },{once: true});
                                                 gr_audiobook_player.addEventListener('timeupdate', () => {
                                                     const playback_time = gr_audiobook_player.currentTime || 0;
+                                                    const now = performance.now();
+                                                    console.log('playback_time', playback_time, 'now', now)
+                                                    if (now - last_time > 3000) {
+                                                        gr_audiobook_player_playback_time.value = String(playback_time);
+                                                        gr_audiobook_player_playback_time.dispatchEvent(new Event("input", { bubbles: true }));
+                                                        last_time = now;
+                                                    }
                                                     const cue = findCue(cues, playback_time);
                                                     if (cue && cue !== lastCue) {
                                                         if (fade_timeout) {
@@ -3941,13 +3948,6 @@ def web_interface(args, ctx):
                                                     } else if (!cue && lastCue !== null) {
                                                         textarea.value = '...';
                                                         lastCue = null;
-                                                    }
-                                                    const now = performance.now();
-                                                    console.log('playback_time', playback_time, 'now', now)
-                                                    if (now - last_time > 3000) {
-                                                        gr_audiobook_player_playback_time.value = String(playback_time);
-                                                        gr_audiobook_player_playback_time.dispatchEvent(new Event("input", { bubbles: true }));
-                                                        last_time = now;
                                                     }
                                                 });
 
